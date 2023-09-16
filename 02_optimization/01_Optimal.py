c92702186c5f14390a94c9042f1df901cdff3976
Hsu0734 <xuhwmy@gmail.com>
2023-09-16 22:29:06 +0200

    Initial commit

--- 02_optimization/01_Optimal.py
+++ 02_optimization/01_Optimal.py
@@ -1,15 +1,14 @@
-'''
-Multi-objective optimization: terrain optimal module
+"""
+Multi-objective optimization: topographic modification optimization
 Author: Hanwen Xu
 Version: 1
 Date: Sep 06, 2023
-'''
+"""
 
 import whitebox_workflows as wbw
 from pymoo.core.problem import ElementwiseProblem
 import numpy as np
 import pandas as pd
-
 
 wbe = wbw.WbEnvironment()
 wbe.verbose = False
@@ -19,9 +18,9 @@ dem = wbe.read_raster('DEM_demo_resample_10m.tif')
 
 # creat a blank raster image of same size as the dem
 cut_and_fill = wbe.new_raster(dem.configs)
+
 # number of valid grid
 n_grid = 0
-
 for row in range(dem.configs.rows):
     for col in range(dem.configs.columns):
         if dem[row, col] == dem.configs.nodata:
@@ -29,6 +28,7 @@ for row in range(dem.configs.rows):
         elif dem[row, col] != dem.configs.nodata:
             cut_and_fill[row, col] = 0.0
             n_grid = n_grid + 1
+print(n_grid)
 
 # ------------------------------------------ #
 # define MOO problem
@@ -51,29 +51,30 @@ class MyProblem(ElementwiseProblem):
 
         # notice your function should be Min function
         earth_volume_function = abs(sum(var_list)) * 100 * 8 / 10000
-        # earth_volume_function = abs(sum(var_list)) * 100 * 8 + sum(abs(i) for i in var_list) * 100 * 4 # grid resolution area: 100  unit price: 5
+        # earth_volume_function = abs(sum(var_list)) * 100 * 8 + sum(abs(i) for i in var_list) * 100 * 4
+        # resolution area: 100m^2  unit price: 5
         flow_length_function = - (path_sum_calculation(var_list))
         velocity_function = velocity_calculation(var_list)
 
-        # notice your funciotn shoube <= 0
+        # notice your function should be <= 0
         g1 = sum(abs(i) for i in var_list) * 100 - 300000
-        g2 = 510 - (path_sum_calculation(var_list))
+        g2 = - (path_sum_calculation(var_list)) + 500
 
         out["F"] = [earth_volume_function, flow_length_function, velocity_function]
         out["G"] = [g1, g2]
 
 def path_sum_calculation(var_list):
-    i = 0
+    p = 0
     for row in range(dem.configs.rows):
         for col in range(dem.configs.columns):
             if dem[row, col] == dem.configs.nodata:
                 cut_and_fill[row, col] = dem.configs.nodata
             elif dem[row, col] != dem.configs.nodata:
                 cut_and_fill[row, col] = var_list[i]
-                i = i + 1
+                p = p + 1
 
     # creat dem_pop
-    dem_pop = dem + cut_and_fill
+    dem_pop = dem - cut_and_fill
 
     # path length calculation
     flow_accum = wbe.d8_flow_accum(dem_pop, out_type='cells')
@@ -81,7 +82,7 @@ def path_sum_calculation(var_list):
 
     for row in range(flow_accum.configs.rows):
         for col in range(flow_accum.configs.columns):
-            elev = flow_accum[row, col] # Read a cell value from a Raster
+            elev = flow_accum[row, col]   # Read a cell value from a Raster
             if elev >= 14.36 and elev != flow_accum.configs.nodata:
                 path_length[row, col] = 1.0
             elif elev < 14.36 or elev == flow_accum.configs.nodata:
@@ -107,7 +108,7 @@ def velocity_calculation(var_list):
                 i = i + 1
 
     # creat dem_pop_02
-    dem_pop_02 = dem + cut_and_fill
+    dem_pop_02 = dem - cut_and_fill
 
     # path length calculation
     flow_accum_02 = wbe.d8_flow_accum(dem_pop_02, out_type='cells')
@@ -121,20 +122,17 @@ def velocity_calculation(var_list):
             if velo == flow_accum_02.configs.nodata:
                 velocity[row, col] = slope.configs.nodata
             elif velo != flow_accum_02.configs.nodata:
-                velocity[row, col] = (((flow_accum_02[row, col] * 100 * 0.000005701259) ** 0.4) * (
-                            (slope[row, col] / 100) ** 0.3)) / ((10 ** 0.4) * (0.03 ** 0.6))
-                #slope_factor = (slope[row, col] / 100) ** 0.5
-                #flow_factor = (flow_accum_02[row, col] * 100 * 0.000005701259) ** (2 / 3)
-                #velocity[row, col] = (slope_factor * flow_factor / 0.03) ** 0.6
+                slope_factor = (slope[row, col] / 100) ** 0.5
+                flow_factor = (flow_accum_02[row, col] * 100 * 0.000004215717) ** (2 / 3)
+                velocity[row, col] = (slope_factor * flow_factor / 0.03) ** 0.6
 
-    Velocity_value = []
+    velocity_value = []
     for row in range(velocity.configs.rows):
         for col in range(velocity.configs.columns):
-            Velocity_value.append(velocity[row, col])
+            velocity_value.append(velocity[row, col])
 
-    max_velocity = max(Velocity_value)
+    max_velocity = max(velocity_value)
     return max_velocity
-
 
 problem = MyProblem(n_grid)
 
@@ -174,7 +172,7 @@ F = res.F
 from pymoo.visualization.scatter import Scatter
 import matplotlib.pyplot as plt
 
-# 3D Visuliaztion
+# 3D Visualization
 plot = Scatter(tight_layout=True)
 plot.add(F, s=10)
 plot.show()
@@ -183,17 +181,17 @@ plot.show()
 plt.figure(figsize=(7, 5))
 plt.scatter(F[:, 0], F[:, 1], s=20, facecolors='none', edgecolors='blue')
 plt.title("Flow path length (y) and total cost (x)")
-plt.grid
+plt.grid()
 plt.show()
 
 plt.scatter(F[:, 1], F[:, 2], s=20, facecolors='none', edgecolors='blue')
 plt.title("Max velocity (y) and flow path length (x)")
-plt.grid
+plt.grid()
 plt.show()
 
 plt.scatter(F[:, 0], F[:, 2], s=20, facecolors='none', edgecolors='blue')
 plt.title("Max velocity (y) and total cost (x)")
-plt.grid
+plt.grid()
 plt.show()
 
 
@@ -207,11 +205,11 @@ min_flow_length = np.argmin(F[:, 1])
 min_velocity = np.argmin(F[:, 2])
 
 min_earth_volume_solution = res.X[min_earth_volume]
-min_flow_lengthe_solution = res.X[min_flow_length]
+min_flow_length_solution = res.X[min_flow_length]
 min_velocity_solution = res.X[min_velocity]
 
 min_earth_volume_dem = wbe.new_raster(dem.configs)
-min_flow_lengthe_dem = wbe.new_raster(dem.configs)
+min_flow_length_dem = wbe.new_raster(dem.configs)
 min_velocity_dem = wbe.new_raster(dem.configs)
 
 wbe.working_directory = r'D:\PhD career\05 SCI papers\05 Lundtoftegade AKB\Lundtoftegade_optimization\03_solution'
@@ -220,21 +218,21 @@ for row in range(dem.configs.rows):
     for col in range(dem.configs.columns):
         if dem[row, col] == dem.configs.nodata:
             min_earth_volume_dem[row, col] = dem.configs.nodata
-            min_flow_lengthe_dem[row, col] = dem.configs.nodata
+            min_flow_length_dem[row, col] = dem.configs.nodata
             min_velocity_dem[row, col] = dem.configs.nodata
 
         elif dem[row, col] != dem.configs.nodata:
             min_earth_volume_dem[row, col] = min_earth_volume_solution[t]
-            min_flow_lengthe_dem[row, col] = min_flow_lengthe_solution[t]
+            min_flow_length_dem[row, col] = min_flow_length_solution[t]
             min_velocity_dem[row, col] = min_velocity_solution[t]
-            t = t + 1
+        t = t + 1
 
 wbe.write_raster(min_earth_volume_dem, file_name='min_earth_volume_solution', compress=True)
-wbe.write_raster(min_flow_lengthe_dem, file_name='min_flow_length_solution', compress=True)
+wbe.write_raster(min_flow_length_dem, file_name='min_flow_length_solution', compress=True)
 wbe.write_raster(min_velocity_dem, file_name='min_velocity_solution', compress=True)
 
 after_dem_minEV = dem - min_earth_volume_dem
-after_dem_minFL = dem - min_flow_lengthe_dem
+after_dem_minFL = dem - min_flow_length_dem
 after_dem_minV = dem - min_velocity_dem
 
 wbe.write_raster(after_dem_minEV, file_name='min_earth_volume_dem', compress=True)
@@ -242,9 +240,7 @@ wbe.write_raster(after_dem_minFL, file_name='min_flow_length_dem', compress=True
 wbe.write_raster(after_dem_minV, file_name='min_velocity_dem', compress=True)
 
 
-
 # visualization of solution set
-wbe.working_directory = r'D:\PhD career\05 SCI papers\05 Lundtoftegade AKB\Lundtoftegade_optimization\03_solution'
 for i in range(20):
     solution = res.X[10 * i] # 每隔十个取一个解
     solution_dem = wbe.new_raster(dem.configs)
